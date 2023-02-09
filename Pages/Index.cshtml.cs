3dfa1843ca28b76b60183ba7029799426bb314a6
Lakshayr04 <124931054+Lakshayr04@users.noreply.github.com>
2023-02-09 16:06:18 -0600

    created webapp

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace _WEB315_Summer22_ThemeDriving.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # -WEB315_Summer22_ThemeDriving
+
+1. Created a PRIVATE github repository that incorporates the course number      WEB315_Summer22_ThemeDriving and Readme.md file created.
+
+2. Invited you as a collaborator to the repository (username is Majid1292).
+
+3. Created webapp and now commiting.
