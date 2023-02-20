3c624562702119acbcbee7757ea2b829638a3283
Lakshayr04 <124931054+Lakshayr04@users.noreply.github.com>
2023-02-20 13:17:59 -0600

    I updated seeddata

--- Models/SeedDataThemeDriving.cs
+++ Models/SeedDataThemeDriving.cs
@@ -26,7 +26,9 @@ namespace _WEB315_Summer22_ThemeDriving.Models
                         BookingDate = DateTime.Parse("2023-2-19"),
                         Route = "University Avenue",
                         Price = 15.54M,
-                        distance = "15 KM"
+                        distance = "15 KM",
+                        colour = "red",
+                        Modelnumber = 2010
                     },
 
                     new ThemeDriving
@@ -35,7 +37,10 @@ namespace _WEB315_Summer22_ThemeDriving.Models
                         BookingDate = DateTime.Parse("2023-5-25"),
                         Route = "XYZ",
                         Price = 21.00M,
-                        distance = "10 KM"
+                        distance = "10 KM",
+                        colour = "white",
+                        Modelnumber = 2015
+                    
                     },
 
                     new ThemeDriving
@@ -44,7 +49,9 @@ namespace _WEB315_Summer22_ThemeDriving.Models
                         BookingDate = DateTime.Parse("2023-06-10"),
                         Route = "YYY",
                         Price = 20.90M,
-                        distance = "12 KM"
+                        distance = "12 KM",
+                        colour = "grey",
+                        Modelnumber = 2012
                     },
 
                     new ThemeDriving
@@ -53,15 +60,20 @@ namespace _WEB315_Summer22_ThemeDriving.Models
                         BookingDate = DateTime.Parse("2023-08-28"),
                         Route = "QAA",
                         Price = 19.44M,
-                        distance = "19 KM"
+                        distance = "19 KM",
+                        colour = "orange",
+                        Modelnumber = 2018
                     },
+
                     new ThemeDriving
                     {
                         CarModel = "A4",
                         BookingDate = DateTime.Parse("2023-03-20"),
                         Route = "ABC",
                         Price = 16.04M,
-                        distance = "24 KM"
+                        distance = "24 KM",
+                        colour = "black",
+                        Modelnumber = 2020
                     }
                 );
                 context.SaveChanges();
