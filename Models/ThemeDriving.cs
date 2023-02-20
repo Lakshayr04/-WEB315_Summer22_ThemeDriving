c1f6c5966e81baf3eef19dcf9e81244f749c81d2
Lakshayr04 <124931054+Lakshayr04@users.noreply.github.com>
2023-02-20 12:54:44 -0600

    I added one string and int

--- Models/ThemeDriving.cs
+++ Models/ThemeDriving.cs
@@ -13,6 +13,8 @@ namespace _WEB315_Summer22_ThemeDriving.Models
         public string Route { get; set; }
         public decimal Price { get; set; }
         public string distance { get; set; }
+        public string colour { get; set; }
+        public int model number { get; set; }
 
         /*public int Rating { get; set;}
         public string RelatedFilms {get; set;}*/
